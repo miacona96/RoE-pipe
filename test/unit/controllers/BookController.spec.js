bb3d580355d75850234921e19d2a49c87c1c212f
miacona96 <miacona@stevens.edu>
2019-03-04 13:04:45 -0500

    test to fetch a book

--- test/unit/controllers/BookController.spec.js
+++ test/unit/controllers/BookController.spec.js
@@ -3,31 +3,69 @@ var util = require('util')
 
 require('../../lifecycle.test')
 
-describe('The Book Controllers', function() {
+describe('The Book Controller', function() {
     
+    /*
+    it("should respond with 200 id is authorised", async () => {
+        const result = await request(app).post("/api/publish")
+            .send({
+                product_id: "123",
+                origin: "localhost:3000",
+            })
+            .expect("Content-Type", /json/)
+            .expect(200);
+
+    });
+    */
+
+    //Test for publishing a new book version
     var createdBookId = 0
-    it('Should create a book', function (done) {
+    it('Should create a book record', function (done) {
         var agent = supertest.agent(sails.hooks.http.app);
         agent
-        .post('/publish')
-        .set('Accept', 'application/json')
-        .send({"title": "a title", "body": "some body"})
-        .expect('Content-Type', /text/)
-        .expect(200)
-        .end(function (err, result) {
-            if (err) {
-                done(err);
-            } 
-            else {
-                result.body.should.be.an('object');
-                result.body.should.have.property('id');
-                result.body.should.have.property('title', 'a title');
-                result.body.should.have.property('author', 'some author');
-                result.body.should.have.property('isbn', 'some isbn');
-                result.body.should.have.property('version', 'some version');
-                createdBookId = result.body.id;
-                done();
-            }
-        });
+            .post('api/publish')
+            .set('Accept', 'application/json')
+            .send({"header": "some key", "body": "some body"})
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function (err, result) {
+                if (err) {
+                    done(err);
+                } 
+                else {
+                    result.body.should.be.an('object');
+                    result.body.should.have.property('created_at', 'timestamp');
+                    result.body.should.have.property('updated_at', 'timestamp');
+                    result.body.should.have.property('id');
+                    result.body.should.have.property('title', 'a title');
+                    result.body.should.have.property('author', 'some author');
+                    result.body.should.have.property('isbn', 'some isbn');
+                    result.body.should.have.property('version', 'some version');
+                    createdBookId = result.body.id;
+                    done();
+                }
+            });
         })
+
+        //test for fetching published books
+        it('Should get published books matching string parameters', function(done){
+            var agent = supertest.agent(sails.hooks.http.app);
+            agent
+                .get('api/books')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function (err, result) {
+                    if (err) {
+                        done(err);
+                    } 
+                    else {
+                        result.body.should.be.an('array') //a list with a json object
+                        result.body.should.have.length(1)
+                        done();
+                    }
+                });
+        })
+
+
 })
